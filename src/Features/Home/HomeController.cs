7a6c4fa461c8ff29dc3916cd806b6b276a67b587
Patrick Herman Andrade da Conceicao Ens <patrickhacens@gmail.com>
2017-12-08 11:25:39 -0200

    Home Controller

---
+++ src/Features/Home/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Preenactos.Home
+{
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public string Get() => "Hello Preenactos";
+    }
+}
