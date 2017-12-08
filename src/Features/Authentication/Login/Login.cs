617b55230934ae7ef5edd34a4674da349d9c0eea
Patrick Herman Andrade da Conceicao Ens <patrickhacens@gmail.com>
2017-12-08 11:53:17 -0200

    Login

---
+++ src/Features/Authentication/Login/Login.cs
@@ -0,0 +1,86 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Preenactos.Infraestructure;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Preenactos.Features.Authentication.Login
+{
+    public class Login
+    {
+        public class Command : IRequest<Result>
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(c => c.Username).NotNull().NotEmpty();
+                RuleFor(c => c.Password).NotNull().NotEmpty();
+            }
+        }
+
+        public class Result
+        {
+            public string Token { get; set; }
+
+            public DateTime Expiration { get; set; }
+        }
+
+        public class Handler : IAsyncRequestHandler<Command, Result>
+        {
+            private readonly Db db;
+            private readonly IConfiguration configuration;
+
+            public Handler(Db db, IStartup startup, IConfiguration configuration)
+            {
+                this.db = db;
+                if (startup is Startup s)
+                    this.configuration = s.Configuration;
+                else this.configuration = configuration;
+            }
+
+            public async Task<Result> Handle(Command message)
+            {
+                Domain.User user = await db.Users.SingleOrDefaultAsync(usern => usern.Email == message.Username);
+
+                if (user == null || !user.IsPasswordEqualsTo(message.Password)) throw new HttpException(401);
+
+                //if (user.DeletedAt != null) throw new UnauthorizedException();
+
+                var claims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Tokens:Key"]));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(configuration["Tokens:Issuer"],
+                                                  configuration["Tokens:Audience"],
+                                                  claims,
+                                                  expires: DateTime.Now.AddYears(1),
+                                                  signingCredentials: creds);
+
+                return new Result
+                {
+                    Token = new JwtSecurityTokenHandler().WriteToken(token),
+                    Expiration = token.ValidTo
+                };
+            }
+        }
+    }
+}
---
+++ src/Features/Authentication/Login/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Preenactos.Infraestructure;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Preenactos.Features.Authentication.Login
+{
+    [Route("api/[controller]")]
+    public class LoginController : Controller
+    {
+        private IMediator mediator;
+
+        public LoginController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<Login.Result> Login([FromBody] Login.Command value)
+        {
+            var result = await mediator.Send(value);
+
+            return result;
+        }
+    }
+}
